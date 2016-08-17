09503c1dd480228e03c8fd5ab2d700c1059b2547
luma <stubma@gmail.com>
2016-08-17 17:09:16 +0800

    export asset bundle, not done

---
+++ TestProject/Assets/LuaLu/Editor/ExportLuaAssetBundles.cs
@@ -0,0 +1,10 @@
+﻿namespace LuaLu {
+	using UnityEditor;
+
+	public class ExportLuaAssetBundles {
+		[MenuItem("Lua/Build AssetBundles")]
+		static void BuildAllAssetBundles() {
+			BuildPipeline.BuildAssetBundles("Assets/StreamingAssets", BuildAssetBundleOptions.None, BuildTarget.StandaloneOSXUniversal);
+		}
+	}
+}
