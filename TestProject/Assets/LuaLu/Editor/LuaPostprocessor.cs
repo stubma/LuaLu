bd83300f89da5c42510e891d0bc22a9ae59dfd8d
luma <stubma@gmail.com>
2016-08-17 23:25:32 +0800

    support export lua asset bundles

--- TestProject/Assets/LuaLu/Editor/LuaPostprocessor.cs
+++ TestProject/Assets/LuaLu/Editor/LuaPostprocessor.cs
@@ -5,9 +5,9 @@
 	using System.IO;
 
 	public class LuaPostprocessor : AssetPostprocessor {
-		private const string CORE_LUA_PREFIX = "Assets/LuaLu/Resources/";
-		private const string USER_LUA_PREFIX = "Assets/Resources/";
-		private const string GENERATED_LUA_PREFIX = "Assets/Generated/Resources/";
+		public const string CORE_LUA_PREFIX = "Assets/LuaLu/Resources/";
+		public const string USER_LUA_PREFIX = "Assets/Resources/";
+		public const string GENERATED_LUA_PREFIX = "Assets/Generated/Resources/";
 
 		private static bool GenerateLuaTextAsset(string asset) {
 			if(asset.EndsWith(".lua")) {
