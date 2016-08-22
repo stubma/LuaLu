6f008147f65c536b9225711c13346bcae129a5da
luma <stubma@gmail.com>
2016-08-22 15:42:43 +0800

    clear object when weak reference is invalid

--- TestProject/Assets/LuaLu/Core/NativeObjectMap.cs
+++ TestProject/Assets/LuaLu/Core/NativeObjectMap.cs
@@ -3,6 +3,7 @@
 	using System.Collections;
 	using System.Collections.Generic;
 	using System;
+	using LuaInterface;
 
 	public class NativeObjectMap {
 		private static Dictionary<int, WeakReference> s_hashObjMap;
@@ -11,12 +12,23 @@
 			s_hashObjMap = new Dictionary<int, WeakReference>();
 		}
 
+		public static void ClearObject(int hash) {
+			if(s_hashObjMap.ContainsKey(hash)) {
+				LuaLib.toluafix_remove_object_by_refid(LuaStack.SharedInstance().GetLuaState(), hash);
+				s_hashObjMap.Remove(hash);
+			}
+		}
+
 		public static object FindObject(int hash) {
 			if(s_hashObjMap.ContainsKey(hash)) {
 				WeakReference r = s_hashObjMap[hash];
 				if(r.IsAlive) {
 					return r.Target;
 				} else {
+					// clear this object
+					ClearObject(hash);
+
+					// return null
 					return null;
 				}
 			} else {
@@ -32,9 +44,20 @@
 			int hash = obj.GetHashCode();
 			if(!s_hashObjMap.ContainsKey(hash)) {
 				s_hashObjMap[hash] = new WeakReference(obj);
+			} else {
+				// check if the refresh is same as obj, if not same, need a clear
+				WeakReference r = s_hashObjMap[hash];
+				if(r.IsAlive) {
+					if(!r.Target.Equals(obj)) {
+						Debug.Log("A collision occurs when register object: " + obj + ", old object is cleared");
+						ClearObject(hash);
+						s_hashObjMap[hash] = new WeakReference(obj);
+					}
+				} else {
+					ClearObject(hash);
+					s_hashObjMap[hash] = new WeakReference(obj);
+				}
 			}
-
-			// TODO think about collision
 		}
 	}
 }
