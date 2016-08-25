eddf70856f4ea4026fd33a1f8449a3d3879defac
luma <stubma@gmail.com>
2016-08-25 20:48:08 +0800

    wrong name

--- TestProject/Assets/Resources/lua/PickUpController.lua
+++ TestProject/Assets/Resources/lua/PickUpController.lua
@@ -1,8 +1,8 @@
 import("UnityEngine")
 
-PlayerController = class("PlayerController", function() return LuaLu.LuaComponent.new() end)
+PickUpController = class("PickUpController", function() return LuaLu.LuaComponent.new() end)
 
-function PlayerController:Update()
+function PickUpController:Update()
   local v = Vector3.new(15 * Time.deltaTime, 30 * Time.deltaTime, 45 * Time.deltaTime)
   self.transform:Rotate(v)
 end
