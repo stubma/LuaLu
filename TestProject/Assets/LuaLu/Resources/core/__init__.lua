9bfb049b15bc387dc877695bfca7ddf23566b32d
luma <stubma@gmail.com>
2016-08-26 15:29:21 +0800

    format

--- TestProject/Assets/LuaLu/Resources/core/__init__.lua
+++ TestProject/Assets/LuaLu/Resources/core/__init__.lua
@@ -26,35 +26,35 @@ end
 
 -- import so that you can omit namespace when use a class
 function import(pkg)
-	-- find table to be imported
-	local nsList = string.split(pkg, ".")
-	local t = _G
-	for _,ns in ipairs(nsList) do
-		t = t[ns]
-		if t == nil then
-			u3d.log("can not find package to import: " .. pkg)
+    -- find table to be imported
+    local nsList = string.split(pkg, ".")
+    local t = _G
+    for _,ns in ipairs(nsList) do
+        t = t[ns]
+        if t == nil then
+            u3d.log("can not find package to import: " .. pkg)
 			return
 		end
 	end
-  
-  -- if has old env, copy it
-  local e = {}
-  local old = getfenv(2)
-  if old ~= nil then
-    for k,v in pairs(old) do
-      e[k] = v
+    
+    -- if has old env, copy it
+    local e = {}
+    local old = getfenv(2)
+    if old ~= nil then
+        for k,v in pairs(old) do
+            e[k] = v
+        end
     end
-  end
-  
-  -- copy the package imported
-  for k,v in pairs(t) do
-    if e[k] ~= nil then
-      u3d.log("env key '" .. k .. "' exists, will be overrided by new import")
+    
+    -- copy the package imported
+    for k,v in pairs(t) do
+        if e[k] ~= nil then
+            u3d.log("env key '" .. k .. "' exists, will be overrided by new import"
+        end
+        e[k] = v
     end
-    e[k] = v
-  end
-
-	-- set env
-	setmetatable(e, { __index = _G, __newindex = _G })
-	setfenv(2, e)
+    
+    -- set env
+    setmetatable(e, { __index = _G, __newindex = _G })
+    setfenv(2, e)
 end
