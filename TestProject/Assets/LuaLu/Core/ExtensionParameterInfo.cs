e12afaf9b39d0f91a4872a424728595d313553e5
luma <stubma@gmail.com>
2016-08-28 18:14:49 +0800

    add IsParams for parameter info

---
+++ TestProject/Assets/LuaLu/Core/ExtensionParameterInfo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Reflection;
+using System;
+
+public static class ExtensionParameterInfo {
+	public static bool IsParams(this ParameterInfo p) {
+		return p.GetCustomAttributes(typeof(ParamArrayAttribute), false).Length > 0;
+	}
+}
